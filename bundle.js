b47be668396ae37bf5c2a413bc2595961fdc671f
Johann <126308696+joh-ann@users.noreply.github.com>
2023-09-19 08:20:06 -0700

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -469,7 +469,7 @@ function displayTags(recipes) {
   const tags = (0,_src_functions_js__WEBPACK_IMPORTED_MODULE_0__.returnListOfUniqueTags)(recipes);
   let tagsHtml = "";
   tags.forEach((tagEl) => {
-    tagsHtml += `<div><img tabindex="0" class="tag-btn" alt="${tagEl}" id="${tagEl}" src="/images/${tagEl}.png"><p>${tagEl}</p></div>
+    tagsHtml += `<div><img tabindex="0" class="tag-btn" alt="${tagEl}" id="${tagEl}" src="https://joh-ann.github.io/whats-cookin/images/${tagEl}.png"><p>${tagEl}</p></div>
     `;
   });
   tagButtons.innerHTML = tagsHtml;
